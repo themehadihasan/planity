7c8e568941142e24ab09a562f2ba476762363b9d
Mehadi Hasan <themehadihasan@gmail.com>
2024-06-07 10:26:24 +0530

    Update line background

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -2,38 +2,44 @@ import React from "react";
 import "../hero/Hero.css";
 import RightImage from "../../../assets/peace.png";
 import ShortCardImage from "../../../assets/peace-reverse.png";
+import LineImage from "../../../assets/line-2.png";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="heroLeft">
-        <h1>
-          Make your <br /> home <br />
-          <span>beautiful</span> <br />
-          with plants
-        </h1>
-        <p>
-          A cozy home is a home decorated with the freshness of green and fresh
-          plants, come on, check out all our plants, don't run out.
-        </p>
-        <button>Learn More</button>
-      </div>
-      <div className="heroRight">
-        <div className="heroRightLeft">
-          <img src={RightImage} alt="Plant-Image" />
+    <>
+      <div className="hero">
+        <div className="lineBackground">
+          <img src={LineImage} alt="Line Image" />
         </div>
-        <div className="shortCard">
-          <span className="shortCardContent">
-            <h4>Pisum Sativum</h4>
-            <p>Monsera</p>
-            <button>LEARN MORE</button>
-          </span>
-          <span className="shortCardImage">
-            <img src={ShortCardImage} alt="Short Card Image" />
-          </span>
+        <div className="heroLeft">
+          <h1>
+            Make your <br /> home <br />
+            <span>beautiful</span> <br />
+            with plants
+          </h1>
+          <p>
+            A cozy home is a home decorated with the freshness of green and
+            fresh plants, come on, check out all our plants, don't run out.
+          </p>
+          <button>Learn More</button>
+        </div>
+        <div className="heroRight">
+          <div className="heroRightLeft">
+            <img src={RightImage} alt="Plant-Image" />
+          </div>
+          <div className="shortCard">
+            <span className="shortCardContent">
+              <h4>Pisum Sativum</h4>
+              <p>Monsera</p>
+              <button>LEARN MORE</button>
+            </span>
+            <span className="shortCardImage">
+              <img src={ShortCardImage} alt="Short Card Image" />
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
