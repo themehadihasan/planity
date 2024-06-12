952543263b8ae40ed93a87d01562079d58b2f824
Mehadi Hasan <themehadihasan@gmail.com>
2024-06-12 13:49:10 +0530

    Remove unused file

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,14 +4,12 @@ import SubHero from "./subhero/SubHero";
 import Products from "./products/Products";
 import Features from "./features/Features";
 import Testimonial from "./testimonial/Testimonial";
-// import Sub from "./sub/Sub";
 
 const Home = () => {
   return (
     <div className="home">
       <Hero />
       <SubHero />
-      {/* <Sub /> */}
       <Products />
       <Features />
       <Testimonial />
